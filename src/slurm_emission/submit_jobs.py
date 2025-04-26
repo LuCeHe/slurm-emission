bddb417c9892a78afa9da1071e68eb579558b01a
Luca <luca.herrtti@gmail.com>
2025-04-26 19:32:52 +0200

    minor changes

--- src/slurm_emission/submit_jobs.py
+++ src/slurm_emission/submit_jobs.py
@@ -1,61 +1,86 @@
-import os, itertools, string, socket, random
-from datetime import datetime, timedelta
+import os, itertools, time, socket, random
 import numpy as np
+from CCsubmit.helpers import get_subset
 
 
 def run_experiments(
-        experiments=None, subset=[0, None], init_command='python language_main.py ',
-        run_string=None, is_argparse=True, sh_save_dir='', account='',
-        duration={'days': 0, 'hours': 12, 'minutes': 0},
-        n_gpus=0, id='', mem='8G', cpus_per_task=2, mock_send=False,
-        bash_prelines='',
-        shs_config_lines=None,
+        experiments=None, subset=None, init_command='python main.py ',
+        run_string=None, is_argparse=True, sh_location='', bash_prelines='', id='', mock_send=False,
+        randomize_seed=0, prevent=[], sbatch_args={}, remove_duplicates=True
 ):
-    """
-    :param experiments: list of dictionaries, each dictionary contains the hyperparameters to be tested
-    :param subset: list of two integers, the first is the start index and the second is the end index
-    :param init_command: the command to be run for each experiment
-    :param run_string: in case you want to run the experiment without sbatch for example
-    :param is_argparse: if the init_command has argparse arguments, so -- has to be added
-    :param sh_save_dir: the directory to save the .sh that will control the jobs
-    :param account: the account to be used in the sbatch
-    :param duration: the duration of the job
-    :param n_gpus: the number of gpus to be used
-    :param id: the id of the job
-    :param mem: the memory to be used
-    :param cpus_per_task: the number of cpus per task
-    :param mock_send: if you want to print the commands instead of running them
-    :param bash_prelines: the bash lines to be run before the experiment
-    :param shs_config_lines: the sbatch lines to be run before the experiment, if you want to parameters that are not
-    in our default configuration
-    """
-    delta = timedelta(days=duration['days'], hours=duration['hours'], minutes=duration['minutes'])
 
-    hours, remainder = divmod(delta.total_seconds(), 3600)
-    minutes, seconds = divmod(remainder, 60)
-    sh_duration = "{}:{}:00".format(str(int(hours)).zfill(2), str(int(minutes)).zfill(2))
+    if isinstance(randomize_seed, int):
+        random.seed(randomize_seed)
+        np.random.seed(randomize_seed)
 
     if run_string is None:
-        sh_name = create_sbatch_sh(sh_duration, sh_save_dir, account, n_gpus, id, mem=mem,
-                                   cpus_per_task=cpus_per_task, bash_prelines=bash_prelines,
-                                   shs_config_lines=shs_config_lines)
+        sh_name = create_sbatch_sh(
+            sh_location=sh_location, id=id,
+            bash_prelines=bash_prelines,
+            sbatch_args=sbatch_args,
+        )
         run_string = 'sbatch ' + sh_name
 
-    if not experiments is None:
+    if not experiments is None and not isinstance(experiments, int):
+        # flatten list of dictionaries
         ds = dict2iter(experiments)
 
+        # convert to string for argparse and to simpler repeat removal
+        new_ds = []
+        a = '--' if is_argparse else ''
+        for d in ds:
+            new_d = ''.join([a + '{}={} '.format(k, v) for k, v in d.items()])
+            new_ds.append(new_d)
+        ds = new_ds
+
+        # remove repeats
+        if remove_duplicates:
+            ds = list(set(ds))
+
+        # remove unwanted combinations
+        new_ds = []
+        for d in ds:
+            elements = d.split(' ')
+            to_include = True
+            for prevention in prevent:
+                assert len(prevention.keys()) == 2
+                k1, v1 = list(prevention.items())[0]
+                k2, v2 = list(prevention.items())[1]
+                e1 = [e for e in elements if e.replace('--', '').startswith(k1)][0]
+                e2 = [e for e in elements if e.replace('--', '').startswith(k2)][0]
+                if v1 in e1 and v2 in e2:
+                    to_include = False
+                    break
+            if to_include:
+                new_ds.append(d)
+
+        ds = new_ds
+
+    elif isinstance(experiments, int):
+        ds = ['' for _ in range(experiments)]
     else:
         ds = ['']
 
-    if isinstance(subset, dict):
+    if subset is None:
+        subset = [0, None]
+
+    elif subset == True:
+        subset, _ = get_subset(ds)
+
+    elif 'DESKTOP' in socket.gethostname():
+        subset = [0, None]
+
+    elif isinstance(subset, dict):
         servers = [k for k, v in subset.items()]
         probs = [v for k, v in subset.items()]
         cumprobs = np.cumsum(probs)
 
+        current_server = socket.gethostname()
+
         amount = len(ds)
         server_found = False
         for i, server in enumerate(servers):
-            if server in socket.gethostname():
+            if server in current_server or (server == 'perlmutter' and not os.getenv("NERSC_PYMON_UUID") is None):
                 server_found = True
                 if not probs[i] == 0:
                     cp = cumprobs[i]
@@ -76,8 +101,8 @@ def run_experiments(
         if not server_found:
             subset = [0, 0]
 
-    random.seed(0)
-    random.shuffle(ds)
+    if isinstance(randomize_seed, int):
+        random.shuffle(ds)
 
     ods = ds
     ds = ds[subset[0]:subset[1]]
@@ -85,13 +110,7 @@ def run_experiments(
     if len(ds) > 0:
         print(f'Number jobs: {len(ds)}/{len(ods)}')
         for i, d in enumerate(ds):
-            if not experiments is None:
-                a = '--' if is_argparse else ''
-                config_update = ''.join([a + '{}={} '.format(k, v) for k, v in d.items()])
-                command = init_command + config_update
-            else:
-                command = init_command
-
+            command = init_command + d
             command = "{} '{}'".format(run_string, command)
             command = command.replace('  ', ' ')
             print('{}/{}'.format(i + 1, len(ds)), command)
@@ -113,34 +132,26 @@ def dict2iter(experiments, to_list=False):
 
 
 def create_sbatch_sh(
-        duration, sh_location, account, n_gpus, id, mem='32G', cpus_per_task=4,
-        bash_prelines='', shs_config_lines=None):
-    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    rand_5 = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+        sh_location, id,
+        bash_prelines='', sbatch_args={}
+):
+    import random
+    named_tuple = time.localtime()  # get struct_time
+    time_string = time.strftime("%Y-%m-%d--%H-%M-%S--", named_tuple)
+    random_string = ''.join([str(random.randint(0, 9)) for _ in range(4)])
 
-    sh_name = f'{id}--' + now + rand_5 + '.sh'
+    sh_name = f'{id}--' + time_string + random_string + '.sh'
     sh_path = os.path.join(sh_location, sh_name)
     with open(sh_path, 'w') as f:
-        sh_string = sh_base(
-            duration, account, n_gpus, mem, cpus_per_task=cpus_per_task,
-            bash_prelines=bash_prelines, shs_config_lines=shs_config_lines
+        f.write(
+            sh_base(bash_prelines=bash_prelines, sbatch_args=sbatch_args)
         )
-        f.write(sh_string)
     return sh_path
 
 
 def sh_base(
-        time, account, n_gpus, mem='32G', cpus_per_task=4,
-        bash_prelines='module load gcc/9.3.0 arrow cuda/11.1 python/3.9 scipy-stack StdEnv/2020',
-        shs_config_lines=None
+        bash_prelines='',
+        sbatch_args={}
 ):
-    gpus_line = '' if n_gpus == 0 else f'#SBATCH --gres=gpu:{n_gpus}'
-    if shs_config_lines is None:
-        shs_config_lines = f"""
-#SBATCH --time={time}
-#SBATCH --account={account}
-#SBATCH --mem {mem}
-#SBATCH --cpus-per-task {cpus_per_task}
-{gpus_line}
-"""
-    return f"#!/bin/bash{shs_config_lines}\n{bash_prelines}\n$1"
+    sbatch_args_line = ''.join([f'#SBATCH --{k}={v}\n' for k, v in sbatch_args.items()])
+    return f"#!/bin/bash\n{sbatch_args_line}\n{bash_prelines}\n$1"
